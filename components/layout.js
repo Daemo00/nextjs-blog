d05ff91dd4d362a28e39443ae36056bc0441c219
Simone Rubino <daemo00@gmail.com>
2023-01-16 23:29:21 +0100

    pre-rendering with data from file system

--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from './layout.module.css';
-import utilStyles from '../styles/utils.module.css';
-import Link from 'next/link';
+import React from "react";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
 
-const name = 'Your Name';
-export const siteTitle = 'Next.js Sample Website';
+const name = "Your Name";
+export const siteTitle = "Next.js Sample Website";
 
 export default function Layout({ children, home }) {
   return (
@@ -20,7 +20,7 @@ export default function Layout({ children, home }) {
         <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
+            siteTitle
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name="og:title" content={siteTitle} />
@@ -68,4 +68,4 @@ export default function Layout({ children, home }) {
     </div>
   );
 }
-Layout.propTypes
+Layout.propTypes;
